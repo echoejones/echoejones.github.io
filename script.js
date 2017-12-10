ec413690ab67135fea43772b05040d3f57ffa60e
echoejones <echoejones@gmail.com>
2017-12-10 13:45:36 -0800

    Missed some thumbnails

--- script.js
+++ script.js
@@ -1,17 +1,9 @@
-var guest_count = document.getElementById("guest-count");
+var pictures = document.getElementById("photo-grid");
 
-guest_count.addEventListener("input", function(){
-	var current_count = guest_count.value;
-	for (i = 1; i < 11; i++){
-		var guest_name_element = document.getElementById("guest-name" + i).parentElement;
-		if (i <= current_count){
-			if (guest_name_element.classList.contains("hidden")) {
-				guest_name_element.classList.remove("hidden");
-			}
-		} else {
-			if (!guest_name_element.classList.contains("hidden")) {
-				guest_name_element.classList.add("hidden");
-			}
-		}
-	}
-});
+for (var i = 0; i < 345; i++){
+	var img = document.createElement("img");
+	img.src = "thumbnails/Nathan Echoe Wedding ("+ (i + 1) +").jpg";
+	img.className += "thumbnail";
+	
+	pictures.appendChild(img);
+}
